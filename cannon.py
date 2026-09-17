e33fb37b099d88b52a5c0368c063400e602b76e3
ithiel_berrum <a01798308@tec.mx>
2026-09-17 09:20:48 -0600

    feat: increase projectile and target speed

--- cannon.py
+++ cannon.py
@@ -2,24 +2,30 @@ from random import randrange
 from turtle import *
 from freegames import vector
 
+PROJECTILE_SPEED_FACTOR = 1.5
+TARGET_SPEED = 1.0
+
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
 
+
 def tap(x, y):
-    "Respond to screen tap."
+    """Respond to screen tap and launch the projectile."""
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = ((x + 200) / 25) * PROJECTILE_SPEED_FACTOR
+        speed.y = ((y + 200) / 25) * PROJECTILE_SPEED_FACTOR
+
 
 def inside(xy):
-    "Return True if xy within screen."
+    """Return True if xy is within the screen."""
     return -200 < xy.x < 200 and -200 < xy.y < 200
 
+
 def draw():
-    "Draw ball and targets."
+    """Draw projectile and targets."""
     clear()
 
     for target in targets:
@@ -32,15 +38,16 @@ def draw():
 
     update()
 
+
 def move():
-    "Move ball and targets."
+    """Move projectile and targets."""
     if randrange(40) == 0:
         y = randrange(-150, 150)
         target = vector(200, y)
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= TARGET_SPEED
 
     if inside(ball):
         speed.y -= 0.35
@@ -60,6 +67,7 @@ def move():
             return
 
     ontimer(move, 50)
+
 
 setup(420, 420, 370, 0)
 hideturtle()
